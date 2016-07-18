13c17fc65ba0c2f59c45e832782ed3f4c1b68973
blqw <blqw@live.com>
2016-07-18 11:27:54 +0800

    修复一些小问题

---
+++ UnitTest/泛型测试.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using blqw.Web;
+using blqw.Web.Extensions;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        public interface ITranslate : ITranslateAsync
+        {
+            [HttpGet("Bing/MicrosoftTranslator/v1/Translate")]
+            string Translate([Header("Authorization")]string token, string Text, string To);
+        }
+
+        public interface ITranslateAsync
+        {
+            [HttpGet("Bing/MicrosoftTranslator/v1/Translate")]
+            Task<string> TranslateAsync([Header]string Authorization, string Text, string To);
+        }
+
+        const string START = "<d:Text m:type=\"Edm.String\">";
+        const string END = "</d:Text>";
+        const string AUTH_TOKEN = "Basic NjBhYzBhNmQtMzkwMi00YTYwLTlhODItOWVhMDU1OTA0OGVhOmVKNk1UY3FRa2tVMlZISWVrcWFLdFBBVi9yQW56Zi9RVGIzY1NCcHFSQkU9";
+
+        [TestMethod]
+        public void 泛型同步测试()
+        {
+            var text = "hello world";
+            var domain = "https://api.datamarket.azure.com";
+            var trans = HttpGenerator.Create<ITranslate>(domain);
+            var xml = trans.Translate(AUTH_TOKEN, $"'{text}'", "'zh-CHS'");
+            var result = GetText(xml);
+            Assert.AreEqual("世界您好", result);
+        }
+
+
+        [TestMethod]
+        public async Task 泛型异步测试()
+        {
+            var text = "hello world";
+            var domain = "https://api.datamarket.azure.com";
+            var trans = HttpGenerator.Create<ITranslate>(domain);
+            var xml = await trans.TranslateAsync(AUTH_TOKEN, $"'{text}'", "'zh-CHS'");
+            var result = GetText(xml);
+            Assert.AreEqual("世界您好", result);
+        }
+
+
+        static string GetText(string str)
+        {
+            var start = str.IndexOf(START);
+            if (start < 0)
+            {
+                return "翻译失败";
+            }
+            start += START.Length;
+            var end = str.IndexOf(END, start);
+            return str.Substring(start, end - start);
+        }
+    }
+}
