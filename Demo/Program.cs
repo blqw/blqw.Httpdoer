c2530426b9a03d18e94defb126c974480473d6c0
blqw.zhou <fttl_398@126.com>
2016-08-22 15:11:14 +0800

    Update Program.cs

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -27,11 +27,6 @@ namespace Demo
                 Console.WriteLine(Bing.Translate2("hello world"));
                 //var user = new User("03f53a51-3291-11e6-b28c-288023a0fe60");
 
-                //var www = new Httpdoer("http://cmd-internal.tops001.com");
-                //www.Path = "/test/getuser";
-                //www.Body.ContentType = "dfsfdasfdsafdsafdsa/json";
-                //www.Method = HttpRequestMethod.Post;
-                //www.Body.AddModel(user);
 
 
 
@@ -41,12 +36,6 @@ namespace Demo
 
 
 
-
-
-
-
-                //var client = HttpGenerator.Create<IMyTestApi>("http://cmd-internal.tops001.com");
-                //var user = client.GetUser(Guid.Parse("03f53a51-3291-11e6-b28c-288023a0fe60"));
 
 
                 Console.Read();
